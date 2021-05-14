c480ad9ce1217fb761474039c96afd3c178d420b
Oscar López <machochinnio@hotmail.com>
2021-05-13 19:56:19 -0500

    Corrección en la tabla de usuarios

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -20,7 +20,6 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique()->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->string('picture')->nullable();
 
             $table->rememberToken();
             $table->softDeletes();
